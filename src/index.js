647cb13cc0e68d82a41f21590d41f8c2cd9d3acd
ngynphong <124499195+ngynphong@users.noreply.github.com>
2024-05-24 00:26:23 +0700

    test

--- src/index.js
+++ src/index.js
@@ -20,10 +20,15 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+app.get('/search', (req, res) => {
+  console.log(req.query.q)
+  res.render('search');
+});
+
 
 
 //127.0.0.1 - localhost
